f54d67597b2f369d018d22912b0a2dc149e569ac
mourya jyoti <mouryajyoti657@gmail.com>
2025-07-04 00:49:39 +0530

    feat: implement ProductList component with product fetching and display

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Emp from "./empComponents/Emp";
+import ProductList from "./ComponentsPagniation/ProductList";
 
 const App = () => {
   return (
     <div>
-      <Emp />
+      {/* <Emp /> */}
+      <ProductList/>
     </div>
   );
 };
--- src/ComponentsPagniation/ProductList.jsx
+++ src/ComponentsPagniation/ProductList.jsx
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const ProductList = () => {
+  const [productsList, SetProductsList] = useState([]);
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      try {
+        const response = await fetch("https://dummyjson.com/products");
+        const data = await response.json();
+        console.log(data);
+        SetProductsList(data.products);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      }
+    };
+    fetchProduct();
+  }, []);
+
   return (
     <div>
       <h1>Creating the pagnation for the project</h1>
+      {productsList.map((product) => (
+        <ul
+          key={product.id}
+          style={{ border: "1px solid #ccc", margin: "10px", padding: "10px" }}
+        >
+          <h2>{product.title}</h2>
+          <p>{product.description}</p>
+          <p>Price: ${product.price}</p>
+          <img
+            src={product.thumbnail}
+            alt={product.title}
+            style={{ width: "100px", height: "100px" }}
+          />
+        </ul>
+      ))}
     </div>
   );
 };
